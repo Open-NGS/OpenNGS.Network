be5de8e10722ad91282c503d02980698486cc695
kanegu@eegames.cn <kanegu@eegames.cn>
2024-01-17 15:16:47 +0800

    调用变更

--- OpenNGS.Game.Systems/RankSystem/RankSystem.cs
+++ OpenNGS.Game.Systems/RankSystem/RankSystem.cs
@@ -21,11 +21,12 @@ namespace OpenNGS.Systems
         }
 
         #region C2S
-        public async void RequestRank(uint nLevelID, OpenNGS.Rank.Common.RANK_DIFFICULT_TYPE _typ)
+        //public async void RequestRank(uint nLevelID, OpenNGS.Rank.Common.RANK_DIFFICULT_TYPE _typ)
+        public void RequestRank(uint nLevelID, OpenNGS.Rank.Common.RANK_DIFFICULT_TYPE _typ)
         {
+            //之后会用异步方法获取数据
             //var rsp = await RankService.Instance.RankRequest(rankId);
-            var rsp = new GetRankRsq();
-            OnRankRsp(rsp);
+            //OnRankRsp(rsp);
         }
 
         #endregion
