75f71f64749a8ab5d9085995b9aef97051a2981c
Mashiro <baiyu_rczy@163.com>
2024-01-18 11:46:43 +0800

    提交商店系统和交易系统

---
+++ OpenNGS.Game.Systems/ExchangeSystem/ExchangeSystem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenNGS.Exchange.Data;
+
+namespace OpenNGS.Systems
+{
+    public class ExchangeSystem : EntitySystem
+    {
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+        }
+
+        public override string GetSystemName()
+        {
+            return "com.openngs.system.rank";
+        }
+
+        public bool Exchange(List<ExchangeItem> src, List<ExchangeItem> target)
+        {
+            return false;
+        }
+
+    }
+}
