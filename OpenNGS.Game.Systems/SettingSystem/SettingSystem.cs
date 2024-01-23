2770f460446971d39988b5fb6adbd1e00aee22a1
Tiam <2892853138@qq.com>
2024-01-23 11:23:38 +0800

    设置系统

--- OpenNGS.Game.Systems/SettingSystem/SettingSystem.cs
+++ OpenNGS.Game.Systems/SettingSystem/SettingSystem.cs
@@ -1,60 +1,132 @@
+using OpenNGS.Collections.Generic;
 using OpenNGS.Rank.Data;
+using OpenNGS.Setting.Common;
 using OpenNGS.Setting.Data;
 using OpenNGS.Systems;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Security.Cryptography;
 using Unity.VisualScripting.YamlDotNet.Core.Tokens;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.TextCore.Text;
 
 public class SettingSystem : EntitySystem
 {
-    public UnityAction<GetSettingRsq> OnGetSetting;
+    public GetSettingRsq OnGetSetting;
+    public UnityAction<Dictionary<uint, GameSettingLabel>>OnGameSettingLabel;
+    public Dictionary<uint, GameSettingLabel> OnGameSettingLabels;
 
-    public UnityAction<List<GameSettingInfo>> OnGetSettingList;
+    public UnityAction<List<AudioSettingInfo>> Onaudio; // 音频
+    public UnityAction<List<KeyControlSettingInfo>> OnkeyControl;  // 按键
+    public UnityAction<OpenNGS.Setting.Data.ScreenSettingInfo> Onscreen; // 画面
+    public UnityAction<OpenNGS.Setting.Data.Language> Onlanguage; //语言
 
-    public Dictionary<uint,GameSettingLabel> OnGameSettingLabels;
-    public Dictionary<uint,GameSettingInfo> OnGameSettingInfos;
+    public Dictionary<uint, AudioSettingInfo> sadsadsadsInfo; 
 
-    public UnityAction OnSave;
+
+    public SettingInfo settingInfo;
+
+    public AudioSettingInfo audio;
+    public KeyControlSettingInfo keyControl;
+    public Language language;
+
+    public string pathName = "";
 
     public void Init()
     {
-        GetSettingLabel();
-        GetSettingInfo();
+        if (OnGameSettingLabels == null) { OnGameSettingLabels = Table<GameSettingLabel, uint>.map; }
     }
 
-    public Dictionary<uint, GameSettingLabel> GetSettingLabel()
+    // 设置标签
+    public void GetGameSettingLabel()
     {
-        if (OnGameSettingLabels == null)
-        {
-            OnGameSettingLabels = Table<GameSettingLabel, uint>.map;
-            return OnGameSettingLabels;
-        }
-        return OnGameSettingLabels;
+        OnGameSettingLabel.Invoke(OnGameSettingLabels);
     }
 
-    public Dictionary<uint, GameSettingInfo> GetSettingInfo()
+    // 获得音频
+    public void GetAduio()
     {
-        if (OnGameSettingInfos == null)
-        {
-            OnGameSettingInfos = Table<GameSettingInfo, uint>.map;
-            return OnGameSettingInfos;
-        }
-        return OnGameSettingInfos;
-    }
-
-    public void Save(uint characterID, Dictionary<uint, GameSettingInfo> settingInfo)
-    {
-        if (settingInfo != OnGameSettingInfos)
-        {
-            SendGameSetting(characterID, settingInfo);
-        }
+        Onaudio.Invoke(settingInfo.audioSettingInfo);
     }
    
+    // 修改音效
+    public void SetAudio(uint value,bool start,OpenNGS.Setting.Common.ADUIO_TYPE _TYPE)
+    {
+        settingInfo.audioSettingInfo[(int)_TYPE].AduioType = _TYPE;
+        settingInfo.audioSettingInfo[(int)_TYPE].Value = value;
+        settingInfo.audioSettingInfo[(int)_TYPE].Switch = start;
+    }
+    // 获得按键
+    public void GetKeyControl()
+    {
+        OnkeyControl.Invoke(settingInfo.keyControlSettingInfo);
+    }
+
+    // 修改按键
+    public void SetKeyControl(OpenNGS.Setting.Common.KEYVALUE_CONTROL_TYPE _TYPE,string key)
+    {
+        settingInfo.keyControlSettingInfo[(int)_TYPE].Key = key;
+        settingInfo.keyControlSettingInfo[(int)_TYPE].KeyType = _TYPE;
+    }
+
+    // 获得分辨率
+    public void GetResolutionRatio()
+    {
+        Onscreen.Invoke(OnGetSetting.SettingInfo.screenSettingInfo);
+    }
+
+    // 修改垂直同步
+    public void SetVerticalSynchronization(bool start)
+    {
+        settingInfo.screenSettingInfo.VerticalSynchronization = start;
+    }
+
+    // 修改分辨率
+    public void SetResolutionRatio(OpenNGS.Setting.Common.RESOLUTIONRATION_TYPE _TYPE)
+    {
+        settingInfo.screenSettingInfo.ResolutionRatio = _TYPE;
+    }
+    // 获取语言
+    public void GetLanguage()
+    {
+        Onlanguage.Invoke(OnGetSetting.SettingInfo.language);
+    }
+
+    // 修改语言
+    public void SetLanguage(OpenNGS.Setting.Common.LANGUAGE_TYPE _TYPE)
+    {
+        settingInfo.language.languageType = _TYPE;
+    }
+
+    // 保存
+    public void Save()
+    {
+        MakeFile(pathName);
+        SendGameSetting(settingInfo);
+    }
+
+    // 本地Save
+    public void MakeFile(string pathName)
+    {
+        if (File.Exists(pathName))
+        {
+            File.Delete(pathName);
+            FileStream NewText = File.Create(pathName);
+            NewText.Close();
+        }
+        else
+        {
+            FileStream NewText = File.Create(pathName);
+            NewText.Close();
+        }
+    }
+
     #region C2S
     // 发送请求
-    public void RequestGameSetting(uint CharacterID)
+    public void RequestGameSetting()
     {
 
     }
@@ -64,14 +136,24 @@ public class SettingSystem : EntitySystem
     // 请求响应
     public void OnSettingRsp(GetSettingRsq rsp)
     {
-        OnGetSetting?.Invoke(rsp);
-        List <GameSettingInfo> settinginfos = new List <GameSettingInfo>();
-        OnGetSettingList?.Invoke(settinginfos);
+        if (rsp.Result == OpenNGS.Setting.Common.RESULT_TYPE.RESULT_TYPE_SUCCESS)
+        {
+            OnGetSetting = rsp;
+
+            Onaudio.Invoke(rsp.SettingInfo.audioSettingInfo);
+            OnkeyControl.Invoke(rsp.SettingInfo.keyControlSettingInfo);
+            Onscreen.Invoke(rsp.SettingInfo.screenSettingInfo);
+            Onlanguage.Invoke(rsp.SettingInfo.language);
+        }
+        else
+        {
+            Debug.LogError(""+rsp.Result);
+        }
     }
     #endregion
 
-    // 发送
-    public void SendGameSetting(uint CharacterID, Dictionary<uint, GameSettingInfo> settingInfo)
+    // 上传数据
+    public void SendGameSetting(SettingInfo settingInfo)
     {
 
     }
