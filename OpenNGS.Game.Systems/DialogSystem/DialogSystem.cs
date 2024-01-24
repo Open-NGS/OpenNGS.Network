1af6b91f4608c39c5f4b908f95eed7173511d678
Mashiro <baiyu_rczy@163.com>
2024-01-24 12:03:28 +0800

    修改dialogsystem中的bug

--- OpenNGS.Game.Systems/DialogSystem/DialogSystem.cs
+++ OpenNGS.Game.Systems/DialogSystem/DialogSystem.cs
@@ -7,31 +7,31 @@ namespace OpenNGS.Systems
 {
     public class DialogueSystem : EntitySystem
     {
-        public UnityAction<DialogueInstanceArray> OnDialogueResult;
-        protected override void OnCreate()
-        {
-            base.OnCreate();
-        }
+        //public UnityAction<DialogueInstanceArray> OnDialogueResult;
+        //protected override void OnCreate()
+        //{
+        //    base.OnCreate();
+        //}
 
-        public override string GetSystemName()
-        {
-            return "com.openngs.system.dialogue";
-        }
+        //public override string GetSystemName()
+        //{
+        //    return "com.openngs.system.dialogue";
+        //}
 
-        #region C2S
-        public void RequestDialogue(uint dialogueID)
-        {
-            // 模拟异步获取对话数据
+        //#region C2S
+        //public void RequestDialogue(uint dialogueID)
+        //{
+        //    // 模拟异步获取对话数据
 
-        }
-        #endregion
+        //}
+        //#endregion
 
-        #region S2C
-        public void OnDialogueResultReceived(DialogueInstanceArray dialogueResult)
-        {
-            OnDialogueResult?.Invoke(dialogueResult);
-            //模拟服务器数据，并调用UI函数
-        }
-        #endregion
+        //#region S2C
+        //public void OnDialogueResultReceived(DialogueInstanceArray dialogueResult)
+        //{
+        //    OnDialogueResult?.Invoke(dialogueResult);
+        //    //模拟服务器数据，并调用UI函数
+        //}
+        //#endregion
     }
 }
