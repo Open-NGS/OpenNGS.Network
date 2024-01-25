66f9e70e89cc70abca63fded2e4c8d3ad7262459
ray <ray@eegames.cn>
2024-01-25 22:18:44 +0800

    增加依赖注入接口

--- OpenNGS.Game.Systems/Common/BootstrapGameContext.cs
+++ OpenNGS.Game.Systems/Common/BootstrapGameContext.cs
@@ -1,9 +1,21 @@
+using Microsoft.Extensions.DependencyInjection;
+using OpenNGS;
 using System;
 using System.Collections.Generic;
 using Systems;
 
 public class BootstrapGameContext : GameContext
 {
+    public override void Configure(IApplicationBuilder app)
+    {
+        
+    }
+
+    public override void ConfigureServices(IServiceCollection services)
+    {
+        
+    }
+
     protected override void OnInit()
     {
         //Logo, Splash
