f1c18847e619b5a5063741cac65a8a8a48d08006
孙楚晰 <arthursun@eegames.cn>
2024-02-01 19:22:04 +0800

    通用装备系统

--- OpenNGS.Game.Systems/Common/LoginGameContext.cs
+++ OpenNGS.Game.Systems/Common/LoginGameContext.cs
@@ -20,7 +20,7 @@ public class LoginGameContext : GameContext
         services.Add(new ServiceDescriptor(typeof(IShopSystem), new ShopSystem()));
         services.Add(new ServiceDescriptor(typeof(ISettingSystem), new SettingSystem()));
         services.Add(new ServiceDescriptor(typeof(IDialogSystem), new DialogSystem()));
-        services.Add(new ServiceDescriptor(typeof(IEquipSystem), new DialogSystem()));
+        services.Add(new ServiceDescriptor(typeof(IEquipSystem), new EquipSystem()));
     }
 
     protected override void OnInit()
