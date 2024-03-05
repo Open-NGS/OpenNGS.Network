d7160bf953bd60195c371c36a6471a45dd61b07b
Mashiro <baiyu_rczy@163.com>
2024-03-05 12:49:51 +0800

    修改level的bug

--- OpenNGS.Game.Systems/Level/LevelTest.cs
+++ OpenNGS.Game.Systems/Level/LevelTest.cs
@@ -6,49 +6,53 @@ using UnityEngine;
 
 public class LevelTest : MonoBehaviour
 {
-    public int levelID = 0;
-    public LevelProcessSystem levelProcessSystem;
+    /*
+     * 测试脚本
+     */
 
-    private StartCondition startCondition;
-    private EndCondition endCondition;
-    private void Start()
-    {
-        levelProcessSystem = new LevelProcessSystem();
-        levelProcessSystem.SetLevel(levelID);
-        levelProcessSystem.InitStages();
+    //public int levelID = 0;
+    //public LevelProcessSystem levelProcessSystem;
 
-        LevelStageBegin levelStageBegin = levelProcessSystem.lstStages[0].lstStages[0] as LevelStageBegin;
-        OpenStartUIExecution openStartUIExecution = new OpenStartUIExecution();
-        levelStageBegin.lstBeginExecution.Add(openStartUIExecution);
-        CloseStartUIExecution closeStartUIExecution = new CloseStartUIExecution();
-        levelStageBegin.lstEndExecution.Add(closeStartUIExecution);
+    //private StartCondition startCondition;
+    //private EndCondition endCondition;
+    //private void Start()
+    //{
+    //    levelProcessSystem = new LevelProcessSystem();
+    //    levelProcessSystem.SetLevel(levelID);
+    //    levelProcessSystem.InitStages();
 
-        levelProcessSystem.lstStages[0].StartBegin();
-    }
-    private void Update()
-    {
-        if (levelProcessSystem.lstStages[0].currentStageIndex < 3)
-        {
-            levelProcessSystem.UpdateStages(UnityEngine.Time.deltaTime);
-        }
-        else
-        {
-            Debug.Log("关卡结束，跳转主城");
-        }
+    //    LevelStageBegin levelStageBegin = levelProcessSystem.lstStages[0].lstStages[0] as LevelStageBegin;
+    //    OpenStartUIExecution openStartUIExecution = new OpenStartUIExecution();
+    //    levelStageBegin.lstBeginExecution.Add(openStartUIExecution);
+    //    CloseStartUIExecution closeStartUIExecution = new CloseStartUIExecution();
+    //    levelStageBegin.lstEndExecution.Add(closeStartUIExecution);
 
-    }
-    public void StartBotton()
-    {
-        LevelStageBegin levelStageBegin = levelProcessSystem.lstStages[0].lstStages[0] as LevelStageBegin;
-        startCondition = (StartCondition)levelStageBegin.lstUpdateExecution[0].LstCondition[0];
-        startCondition.isStart = true;
-    }
+    //    levelProcessSystem.lstStages[0].StartBegin();
+    //}
+    //private void Update()
+    //{
+    //    if (levelProcessSystem.lstStages[0].currentStageIndex < 3)
+    //    {
+    //        levelProcessSystem.UpdateStages(UnityEngine.Time.deltaTime);
+    //    }
+    //    else
+    //    {
+    //        Debug.Log("关卡结束，跳转主城");
+    //    }
 
-    public void EndBotton()
-    {
-        LevelStageEnd levelStageEnd = levelProcessSystem.lstStages[0].lstStages[2] as LevelStageEnd;
-        endCondition = (EndCondition)levelStageEnd.lstUpdateExecution[0].LstCondition[0];
-        endCondition.isEnd = true;
-    }
+    //}
+    //public void StartBotton()
+    //{
+    //    LevelStageBegin levelStageBegin = levelProcessSystem.lstStages[0].lstStages[0] as LevelStageBegin;
+    //    startCondition = (StartCondition)levelStageBegin.lstUpdateExecution[0].LstCondition[0];
+    //    startCondition.isStart = true;
+    //}
+
+    //public void EndBotton()
+    //{
+    //    LevelStageEnd levelStageEnd = levelProcessSystem.lstStages[0].lstStages[2] as LevelStageEnd;
+    //    endCondition = (EndCondition)levelStageEnd.lstUpdateExecution[0].LstCondition[0];
+    //    endCondition.isEnd = true;
+    //}
 
 }
