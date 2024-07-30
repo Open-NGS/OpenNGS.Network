0cab65a5856f5cb5149fe1ac063b4b70d96b2057
ray <ray@eegames.cn>
2024-07-30 11:03:42 +0800

    增加新脚本

---
+++ pullsubtree.cmd
@@ -0,0 +1,11 @@
+
+git subtree pull --prefix Client/Packages/openngs.core https://git.eegames.net/openngs/client/openngs.core.git main
+git subtree pull --prefix Client/Packages/openngs.data https://git.eegames.net/openngs/client/openngs.data.git main
+git subtree pull --prefix Client/Packages/openngs.di https://git.eegames.net/openngs/client/openngs.di.git main
+git subtree pull --prefix Client/Packages/openngs.erpc https://git.eegames.net/openngs/client/openngs.erpc.git main
+git subtree pull --prefix Client/Packages/openngs.network https://git.eegames.net/openngs/client/openngs.network.git main
+git subtree pull --prefix Client/Packages/openngs.protobuf https://git.eegames.net/openngs/client/openngs.protobuf.git main
+git subtree pull --prefix Client/Packages/openngs.ui https://git.eegames.net/openngs/client/openngs.ui.git main
+git subtree pull --prefix Client/Packages/openngs.platform https://git.eegames.net/openngs/client/openngs.platform.git main
+git subtree pull --prefix Client/Packages/openngs.game https://git.eegames.net/openngs/client/openngs.game.git main
+git subtree pull --prefix Client/Packages/openngs.game.systems https://git.eegames.net/openngs/client/openngs.game.systems.git main
