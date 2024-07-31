7d2ac763f52136de52c4e3f9b9aecc6077c2032b
孙楚晰 <arthursun@eegames.cn>
2024-07-31 15:02:47 +0800

    更新NgSettingSystem字段

--- OpenNGS.Game.Systems/NgSettingSystem/SettingLocalAPI.cs
+++ OpenNGS.Game.Systems/NgSettingSystem/SettingLocalAPI.cs
@@ -5,7 +5,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SettingLocalAPI : Singleton<SettingLocalAPI>,INgSettingSystem
+public class SettingLocalAPI : Singleton<SettingLocalAPI>,ISettingClientAPI
 {
     INgSettingSystem ngSettingSystem = App.GetService<NgSettingSystem>();
     public void AddActionOnSettingChange(Action<int, int> ac)
