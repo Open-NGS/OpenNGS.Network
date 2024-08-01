b181b05db4ace54b5d5af06a66b01fd0db2af90e
lucyfang <lucyfang@eegames.cn>
2024-08-01 16:27:22 +0800

    删除dialogsystem

--- OpenNGS.Game.Systems/Common/WorldGameContext.cs
+++ OpenNGS.Game.Systems/Common/WorldGameContext.cs
@@ -18,7 +18,6 @@ public class WorldGameContext : GameContext
         services.Add(new ServiceDescriptor(typeof(IShopSystem), new ShopSystem()));
         services.Add(new ServiceDescriptor(typeof(ISettingSystem), new SettingSystem()));
         services.Add(new ServiceDescriptor(typeof(IRecordSystem), new RecordSystem()));
-        services.Add(new ServiceDescriptor(typeof(IDialogSystem), new DialogSystem()));
         services.Add(new ServiceDescriptor(typeof(IEquipSystem), new EquipSystem()));
         services.Add(new ServiceDescriptor(typeof(ITechnologySystem), new TechnologySystem()));
         services.Add(new ServiceDescriptor(typeof(IRankSystem), new RankSystem()));
