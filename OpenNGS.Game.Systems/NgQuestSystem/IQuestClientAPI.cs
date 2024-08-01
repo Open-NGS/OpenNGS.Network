ed901d1c3fa1964029f3234e1e20946d313c4500
lucyfang <lucyfang@eegames.cn>
2024-08-01 11:54:20 +0800

    更新ngquestsystem

---
+++ OpenNGS.Game.Systems/NgQuestSystem/IQuestClientAPI.cs
@@ -0,0 +1,15 @@
+using OpenNGS.Quest.Common;
+using OpenNGS.Quest.Data;
+using System.Collections.Generic;
+
+namespace OpenNGS.Systems
+{
+    public interface IQuestClientAPI
+    {
+        public void AddQuestGroup(uint questGroupID);
+        public void FinishQuestGroup(uint questGroupID);
+        public void UpdateQuest(uint questGroupID, uint questID, OpenNGS.Quest.Common.Quest_Status status);
+        public List<uint> GetQuestGroup(Quest_Status status);
+        void AddQuestContainer(QuestContainer Container);
+    }
+}
