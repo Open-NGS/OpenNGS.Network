1dcd52a6ecbd7e0150a76500f04c1c4d8fdae2ec
孙楚晰 <arthursun@eegames.cn>
2024-08-23 13:47:46 +0800

    更新设置存档

--- OpenNGS.Game.Systems/NgSettingSystem/UserSettingContainer.cs
+++ OpenNGS.Game.Systems/NgSettingSystem/UserSettingContainer.cs
@@ -6,7 +6,13 @@ namespace OpenNGS.Setting.Data
 {
     public partial class UserSettingContainer
     {
-
+        public void AddSetting(UserSettingValueState state)
+        {
+            if (ValueState != null)
+            {
+                ValueState.Add(state);
+            }
+        }
         public void ChangeSetting(int settingType, int value)
         {
             if (ValueState != null)
