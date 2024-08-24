22f67af8038b5663b8ef4d474c78822da2865f6e
ray <ray@eegames.cn>
2024-08-24 13:17:34 +0800

    增加Package 清单定义文档

--- pullsubtree.cmd
+++ pullsubtree.cmd
@@ -1,6 +1,7 @@
 
 git subtree pull --prefix Client/Packages/openngs.core https://git.eegames.net/openngs/client/openngs.core.git main
 git subtree pull --prefix Client/Packages/openngs.data https://git.eegames.net/openngs/client/openngs.data.git main
+git subtree pull --prefix Client/Packages/openngs.savedata https://git.eegames.net/openngs/client/openngs.savedata.git main
 git subtree pull --prefix Client/Packages/openngs.di https://git.eegames.net/openngs/client/openngs.di.git main
 git subtree pull --prefix Client/Packages/openngs.erpc https://git.eegames.net/openngs/client/openngs.erpc.git main
 git subtree pull --prefix Client/Packages/openngs.network https://git.eegames.net/openngs/client/openngs.network.git main
--- pushsubtree.cmd
+++ pushsubtree.cmd
@@ -1,6 +1,7 @@
 
 git subtree push --prefix Client/Packages/openngs.core https://git.eegames.net/openngs/client/openngs.core.git main -q
 git subtree push --prefix Client/Packages/openngs.data https://git.eegames.net/openngs/client/openngs.data.git main -q
+git subtree push --prefix Client/Packages/openngs.savedata https://git.eegames.net/openngs/client/openngs.savedata.git main -q
 git subtree push --prefix Client/Packages/openngs.di https://git.eegames.net/openngs/client/openngs.di.git main -q
 git subtree push --prefix Client/Packages/openngs.erpc https://git.eegames.net/openngs/client/openngs.erpc.git main -q
 git subtree push --prefix Client/Packages/openngs.network https://git.eegames.net/openngs/client/openngs.network.git main -q
