6fa1d1b2ec5d4835d114bc14636a74dfd632f145
ray <ray@eegames.cn>
2024-09-03 11:37:49 +0800

    更新包清单

--- pullsubtree.cmd
+++ pullsubtree.cmd
@@ -3,6 +3,7 @@ git subtree pull --prefix Client/Packages/openngs.build https://git.eegames.net/
 git subtree pull --prefix Client/Packages/openngs.core https://git.eegames.net/openngs/client/openngs.core.git main
 git subtree pull --prefix Client/Packages/openngs.data https://git.eegames.net/openngs/client/openngs.data.git main
 git subtree pull --prefix Client/Packages/openngs.savedata https://git.eegames.net/openngs/client/openngs.savedata.git main
+git subtree pull --prefix Client/Packages/openngs.render-piplines https://git.eegames.net/openngs/client/openngs.render-piplines.git main
 git subtree pull --prefix Client/Packages/openngs.di https://git.eegames.net/openngs/client/openngs.di.git main
 git subtree pull --prefix Client/Packages/openngs.erpc https://git.eegames.net/openngs/client/openngs.erpc.git main
 git subtree pull --prefix Client/Packages/openngs.network https://git.eegames.net/openngs/client/openngs.network.git main
