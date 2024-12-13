bb6ac26ba8027d2ab6b9c69aa6464400c5661c5e
lucyfang <lucyfang@eegames.cn>
2024-12-13 11:46:23 +0800

    分离盲盒系统的StaticData

--- OpenNGS.Game.Systems/NgBlindBoxSystem/BlindBoxStaticData.cs
+++ OpenNGS.Game.Systems/NgBlindBoxSystem/BlindBoxStaticData.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 namespace OpenNGS.Systems
 {
-    public static class BindBoxStaticData
+    public static class BlindBoxStaticData
     {
         public static Table<OpenNGS.BlindBox.Data.Drop, uint> drops = new Table<OpenNGS.BlindBox.Data.Drop, uint>((item) => { return item.DropID; }, false);
         public static Table<OpenNGS.BlindBox.Data.DropRule, uint> droprules = new Table<OpenNGS.BlindBox.Data.DropRule, uint>((item) => { return item.DropRuleID; }, false);
