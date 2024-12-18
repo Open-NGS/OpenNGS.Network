bd5ce0c76dc8f8ddcc1247f92c9c173e01449e95
lucyfang <lucyfang@eegames.cn>
2024-12-18 18:21:28 +0800

    重构成就系统

--- OpenNGS.Game.Systems/Generated/Service/OpenNGS.Achievement.Service.cs
+++ OpenNGS.Game.Systems/Generated/Service/OpenNGS.Achievement.Service.cs
@@ -6,6 +6,8 @@
 
 #region Designer generated code
 #pragma warning disable 0612, 0618, 1591, 3021
+using OpenNGS.Achievement.Data;
+
 namespace OpenNGS.Achievement.Service
 {
 
@@ -23,12 +25,11 @@ namespace OpenNGS.Achievement.Service
         }
 
         partial void OnConstructor();
-
         [global::ProtoBuf.ProtoMember(1)]
-        public ulong uin { get; set; }
+        public uint playerID { get; set; }
 
         [global::ProtoBuf.ProtoMember(2)]
-        public uint tid { get; set; }
+        public uint ID { get; set; }
 
     }
 
@@ -48,48 +49,44 @@ namespace OpenNGS.Achievement.Service
         partial void OnConstructor();
 
         [global::ProtoBuf.ProtoMember(1)]
-        public int result { get; set; }
-
-        [global::ProtoBuf.ProtoMember(2)]
-        public global::OpenNGS.Achievement.Common.AchievementInfo achi { get; set; }
+        public global::OpenNGS.Achievement.Common.Achievement_Result result { get; set; }
 
     }
 
     [global::ProtoBuf.ProtoContract()]
-    public partial class AchievementDataChangedReq : global::ProtoBuf.IExtensible
+    public partial class UpdateAchievementReq : global::ProtoBuf.IExtensible
     {
         private global::ProtoBuf.IExtension __pbn__extensionData;
         global::ProtoBuf.IExtension global::ProtoBuf.IExtensible.GetExtensionObject(bool createIfMissing)
         {
             return global::ProtoBuf.Extensible.GetExtensionObject(ref __pbn__extensionData, createIfMissing);
         }
-        public AchievementDataChangedReq()
+        public UpdateAchievementReq()
         {
             OnConstructor();
         }
 
         partial void OnConstructor();
-
         [global::ProtoBuf.ProtoMember(1)]
-        public ulong uin { get; set; }
+        public uint playerID { get; set; }
 
         [global::ProtoBuf.ProtoMember(2)]
-        public uint tid { get; set; }
+        public uint ID { get; set; }
 
         [global::ProtoBuf.ProtoMember(3)]
-        public ulong value { get; set; }
+        public uint progress { get; set; }
 
     }
 
     [global::ProtoBuf.ProtoContract()]
-    public partial class AchievementDataChangedRsp : global::ProtoBuf.IExtensible
+    public partial class UpdateAchievementRsp : global::ProtoBuf.IExtensible
     {
         private global::ProtoBuf.IExtension __pbn__extensionData;
         global::ProtoBuf.IExtension global::ProtoBuf.IExtensible.GetExtensionObject(bool createIfMissing)
         {
             return global::ProtoBuf.Extensible.GetExtensionObject(ref __pbn__extensionData, createIfMissing);
         }
-        public AchievementDataChangedRsp()
+        public UpdateAchievementRsp()
         {
             OnConstructor();
         }
@@ -97,7 +94,47 @@ namespace OpenNGS.Achievement.Service
         partial void OnConstructor();
 
         [global::ProtoBuf.ProtoMember(1)]
-        public int result { get; set; }
+        public global::OpenNGS.Achievement.Common.Achievement_Result result { get; set; }
+
+    }
+
+    [global::ProtoBuf.ProtoContract()]
+    public partial class GetAchievementsReq : global::ProtoBuf.IExtensible
+    {
+        private global::ProtoBuf.IExtension __pbn__extensionData;
+        global::ProtoBuf.IExtension global::ProtoBuf.IExtensible.GetExtensionObject(bool createIfMissing)
+        {
+            return global::ProtoBuf.Extensible.GetExtensionObject(ref __pbn__extensionData, createIfMissing);
+        }
+        public GetAchievementsReq()
+        {
+            OnConstructor();
+        }
+
+        partial void OnConstructor();
+
+        [global::ProtoBuf.ProtoMember(1)]
+        public uint playerID { get; set; }
+
+    }
+
+    [global::ProtoBuf.ProtoContract()]
+    public partial class GetAchievementsRsp : global::ProtoBuf.IExtensible
+    {
+        private global::ProtoBuf.IExtension __pbn__extensionData;
+        global::ProtoBuf.IExtension global::ProtoBuf.IExtensible.GetExtensionObject(bool createIfMissing)
+        {
+            return global::ProtoBuf.Extensible.GetExtensionObject(ref __pbn__extensionData, createIfMissing);
+        }
+        public GetAchievementsRsp()
+        {
+            OnConstructor();
+        }
+
+        partial void OnConstructor();
+
+        [global::ProtoBuf.ProtoMember(1)]
+        public global::System.Collections.Generic.List<AchievementState> achievementStates { get; set; }
 
     }
 
@@ -105,7 +142,8 @@ namespace OpenNGS.Achievement.Service
     public partial interface INiAchievementService
     {
         global::System.Threading.Tasks.Task<global::OpenNGS.Achievement.Service.GetAchievementRewardRsp> GetAchievementReward(global::OpenNGS.Achievement.Service.GetAchievementRewardReq value, global::OpenNGS.ERPC.ClientContext context = default(global::OpenNGS.ERPC.ClientContext));
-        global::System.Threading.Tasks.Task<global::OpenNGS.Achievement.Service.AchievementDataChangedRsp> AchievementDataChanged(global::OpenNGS.Achievement.Service.AchievementDataChangedReq value, global::OpenNGS.ERPC.ClientContext context = default(global::OpenNGS.ERPC.ClientContext));
+        global::System.Threading.Tasks.Task<global::OpenNGS.Achievement.Service.UpdateAchievementRsp> UpdateAchievement(global::OpenNGS.Achievement.Service.UpdateAchievementReq value, global::OpenNGS.ERPC.ClientContext context = default(global::OpenNGS.ERPC.ClientContext));
+        global::System.Threading.Tasks.Task<global::OpenNGS.Achievement.Service.GetAchievementsRsp> GetAchievements(global::OpenNGS.Achievement.Service.GetAchievementsReq value, global::OpenNGS.ERPC.ClientContext context = default(global::OpenNGS.ERPC.ClientContext));
     }
 
 }
---
+++ OpenNGS.Game.Systems/NgAchievementSystem/NgAchievementSystem.cs
@@ -0,0 +1,174 @@
+using OpenNGS;
+using OpenNGS.Achievement.Common;
+using OpenNGS.Achievement.Data;
+using OpenNGS.Achievement.Service;
+using OpenNGS.ERPC;
+using OpenNGS.Systems;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+
+public class NgAchievementSystem : Singleton<NgAchievementSystem>, INgAchievementSystem
+{
+
+    public List<AchievementState> achievementStates { get; set; }
+    private Dictionary<uint, List<uint>> parentAchievementMap = new Dictionary<uint, List<uint>>();
+
+
+    public void AddAchievementStates(List<AchievementState> AchievementStates = null)
+    {
+        if (AchievementStates != null)
+        {
+            achievementStates = AchievementStates;
+        }
+        else
+        {
+            achievementStates = new List<AchievementState>();
+        }
+
+        foreach (var achievement in AchievementStaticData.achievement.Items)
+        {
+            if (achievement.SubAchievement != null)
+            {
+                foreach (var subID in achievement.SubAchievement)
+                {
+                    if (!parentAchievementMap.ContainsKey(subID))
+                    {
+                        parentAchievementMap[subID] = new List<uint>();
+                    }
+                    parentAchievementMap[subID].Add(achievement.ID);
+                }
+            }
+        }
+
+    }
+    public Task<GetAchievementRewardRsp> GetAchievementReward(GetAchievementRewardReq value, ClientContext context = null)
+    {
+        GetAchievementRewardRsp rsp = new GetAchievementRewardRsp();
+
+        AchievementState achievementState = achievementStates
+            .Find(a => a.ID == value.ID);
+        if (achievementState != null)
+        {
+            if(achievementState.status == Achievement_Status.Achievement_Status_Pending)
+            {
+                achievementState.status = Achievement_Status.Achievement_Status_Done;
+                Achievement parentAchievement = AchievementStaticData.achievement.GetItem(value.ID);
+                foreach (var subID in parentAchievement.SubAchievement)
+                {
+                    var subState = achievementStates.Find(a => a.ID == subID);
+                    if (subState == null || subState.status == Achievement_Status.Achievement_Status_Stating)
+                    {
+                        subState.status = Achievement_Status.Achievement_Status_Done;
+                    }
+                }
+                rsp.result = Achievement_Result.AchievementResult_Success;
+            }
+            else if (achievementState.status == Achievement_Status.Achievement_Status_Stating)
+            {
+                rsp.result = Achievement_Result.Achievement_Result_None;
+            }
+            else if (achievementState.status == Achievement_Status.Achievement_Status_Done)
+            {
+                rsp.result = Achievement_Result.AchievementResult_Fail_HasGet;
+            }
+            else
+            {
+                rsp.result = Achievement_Result.Achievement_Result_None;
+            }
+        }
+        else
+        {
+            rsp.result = Achievement_Result.AchievementResult_Fail_NotExist;
+        }
+        return Task.FromResult(rsp);
+    }
+
+    public Task<UpdateAchievementRsp> UpdateAchievement(UpdateAchievementReq value, ClientContext context = null)
+    {
+        UpdateAchievementRsp updateAchievementRsp = new UpdateAchievementRsp();
+        updateAchievementRsp.result = Achievement_Result.Achievement_Result_None;
+
+        AchievementState achievementState = achievementStates
+            .Find(a => a.ID == value.ID);
+        Achievement achievement = AchievementStaticData.achievement.GetItem(value.ID);
+        if (achievementState != null)
+        {
+            achievementState.Progress = value.progress;
+        }
+        else
+        {
+            // 没找到对应成就，添加新的成就状态
+            achievementState = new AchievementState
+            {
+                PlayerID = value.playerID,
+                ID = value.ID,
+                Progress = value.progress,
+                status = Achievement_Status.Achievement_Status_Stating,
+            };
+            achievementStates.Add(achievementState);
+        }
+        if (achievementState.Progress >= achievement.StatValue)
+        {
+            achievementState.status = Achievement_Status.Achievement_Status_Pending;
+            //时间
+            CheckAndUpdateParentAchievements(value.ID, value.playerID);
+        }
+        updateAchievementRsp.result = Achievement_Result.AchievementResult_Success;
+        return Task.FromResult(updateAchievementRsp);
+    }
+    private void CheckAndUpdateParentAchievements(uint subAchievementID,uint player)
+    {
+        if (!parentAchievementMap.ContainsKey(subAchievementID))
+            return;
+
+        List<uint> parentAchievements = parentAchievementMap[subAchievementID];
+        foreach (var parentID in parentAchievements)
+        {
+            Achievement parentAchievement = AchievementStaticData.achievement.GetItem(parentID);
+            AchievementState parentState = achievementStates.Find(a => a.ID == parentID);
+
+            if (parentAchievement == null)
+                continue;
+
+            if (parentState == null)
+            {
+                parentState = new AchievementState
+                {
+                    PlayerID = player,
+                    ID = parentID,
+                    Progress = 0,
+                    status = Achievement_Status.Achievement_Status_Stating,
+                };
+                achievementStates.Add(parentState);
+            }
+
+            bool allSubAchievementsCompleted = true;
+            foreach (var subID in parentAchievement.SubAchievement)
+            {
+                var subState = achievementStates.Find(a => a.ID == subID);
+                if (subState == null || subState.status == Achievement_Status.Achievement_Status_Stating)
+                {
+                    allSubAchievementsCompleted = false;
+                    break;
+                }
+            }
+            if (allSubAchievementsCompleted)
+            {
+                parentState.status = Achievement_Status.Achievement_Status_Done;
+                //时间
+            }
+        }
+    }
+
+    public Task<GetAchievementsRsp> GetAchievements(GetAchievementsReq value, ClientContext context = null)
+    {
+        var response = new GetAchievementsRsp
+        {
+            achievementStates = achievementStates
+        };
+        return Task.FromResult(response);
+    }
+}
+
