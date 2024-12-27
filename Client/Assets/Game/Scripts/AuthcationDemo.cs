777fa2fa339089c00d92424373b156ab55800288
kanegu <kanegu@eegames.com>
2024-12-27 10:26:25 +0800

    登录demo添加

--- Client/Assets/Game/Scripts/AuthcationDemo.cs
+++ Client/Assets/Game/Scripts/AuthcationDemo.cs
@@ -1,15 +1,19 @@
 using OpenNGS.SDK.Auth;
 using OpenNGS.SDK.Core.Initiallization;
 using OpenNGS.SDK.Core;
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using OpenNGS.SDK.Log;
 
 public class AuthcationDemo : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public string UserName;
+    public string Password;
+    public string EmailAddress = "kanegu@eegames.com";
+    public string PhoneNumber = "13311788813";
+    public string VerifyCode;
+
+    [ContextMenu("LoginAccount")]
+    private void LoginAccount()
     {
         Debug.Log("Test Auth ------------------------------- !");
 
@@ -19,31 +23,154 @@ public class AuthcationDemo : MonoBehaviour
             AppSecret = "YDwqSQ5Be0oGIpQJ6sPBlHLHveRTfC5p"
         }, new SDKLogger());
 
-        AuthcationService.Instance.LoginCallback += (info) =>
+        AuthcationService.Instance.LoginCallback += (result) =>
         {
-            Debug.Log($"ÒÑµÇÂ¼ {info.Nickname}");
+            SDKLog.Info($"[ÕËºÅÃÜÂëµÇÂ¼³É¹¦] ÓÃ»§:[{result.Nickname}]");
+        };
+        
+        AuthcationService.Instance.LoginResultCallback += (result) =>
+        {
+            ResultCode _res = (ResultCode)result;
+            if (_res != ResultCode.RESULT_OK)
+            {
+                Debug.Log($"µÇÂ¼´íÎó {_res}");
+            }
         };
 
-        AuthcationService.Instance.VerificationCodeCallback += () =>
+        AuthcationService.Instance.LoginByUsernamePassword(UserName, Password);
+    }
+
+    [ContextMenu("TestAutoLogin")]
+    private void TestAutoLogin()
+    {
+        Debug.Log("Test Auth ------------------------------- !");
+
+        OpenNGSPlatformServices.Initialize(new InitializationOptions()
         {
-            int a = 0;
-        };
+            AppId = "iboN4V3anKRnsKwgudonW0ESxGwJLNUz2rhN",
+            AppSecret = "YDwqSQ5Be0oGIpQJ6sPBlHLHveRTfC5p"
+        }, new SDKLogger());
 
         AuthcationService.Instance.AutoLoginCallback += (result) =>
         {
             if (result)
             {
-                Debug.Log($"ÒÑµÇÂ¼ {AuthcationService.Instance.User.Nickname}");
+                Debug.Log($"ÒÑ×Ô¶¯µÇÂ¼ {AuthcationService.Instance.User.Nickname}");
             }
             else
             {
                 Debug.Log("Î´µÇÂ¼£¬ÕýÔÚµÇÂ½....");
-                AuthcationService.Instance.LoginByUsernamePassword("xxx", "xxx");
+                AuthcationService.Instance.LoginByUsernamePassword(UserName, Password);
             }
         };
 
         AuthcationService.Instance.AutoLogin();
 
         Debug.Log("Test Auth End ---------------------------- !");
+    }
+
+    [ContextMenu("RequestVerifyCodeEmail")]
+    private void RequestVerifyCodeEmail()
+    {
+        OpenNGSPlatformServices.Initialize(new InitializationOptions()
+        {
+            AppId = "iboN4V3anKRnsKwgudonW0ESxGwJLNUz2rhN",
+            AppSecret = "YDwqSQ5Be0oGIpQJ6sPBlHLHveRTfC5p"
+        }, new SDKLogger());
+
+        AuthcationService.Instance.VerificationCodeCallback += (result) =>
+        {
+            SDKLog.Info($"[RequestVerifyCodeEmail] code:[{result}]");
+        };
+        AuthcationService.Instance.SendVerificationCode( OpenNGS.SDK.Auth.Models.VerificationType.Email,
+            EmailAddress);
+    }
+
+
+    [ContextMenu("RequestVerifyCodePhone")]
+    private void RequestVerifyCodePhone()
+    {
+        OpenNGSPlatformServices.Initialize(new InitializationOptions()
+        {
+            AppId = "iboN4V3anKRnsKwgudonW0ESxGwJLNUz2rhN",
+            AppSecret = "YDwqSQ5Be0oGIpQJ6sPBlHLHveRTfC5p"
+        }, new SDKLogger());
+
+        AuthcationService.Instance.VerificationCodeCallback += (result) =>
+        {
+            SDKLog.Info($"[RequestVerifyCodePhone] code:[{result}]");
+        };
+        AuthcationService.Instance.SendVerificationCode(OpenNGS.SDK.Auth.Models.VerificationType.Phone,
+            PhoneNumber);
+    }
+
+    [ContextMenu("TryLoginPhone")]
+    private void TryLoginPhone()
+    {
+        OpenNGSPlatformServices.Initialize(new InitializationOptions()
+        {
+            AppId = "iboN4V3anKRnsKwgudonW0ESxGwJLNUz2rhN",
+            AppSecret = "YDwqSQ5Be0oGIpQJ6sPBlHLHveRTfC5p"
+        }, new SDKLogger());
+
+        AuthcationService.Instance.LoginCallback += (result) =>
+        {
+            SDKLog.Info($"[LoginCallback] ÓÃ»§[{result.Nickname}] µÇÂ¼³É¹¦ code:[{result}]");
+        };
+
+        AuthcationService.Instance.LoginResultCallback += (result) =>
+        {
+            SDKLog.Error($"[LoginCallback] µÇÂ¼´íÎó code:[{result}]");
+        };
+
+
+        AuthcationService.Instance.LoginOrRegisterByVerificationCode(
+            OpenNGS.SDK.Auth.Models.VerificationType.Phone,
+            PhoneNumber,
+            VerifyCode);
+    }
+
+    [ContextMenu("TryLoginEmail")]
+    private void TryLoginEmail()
+    {
+        OpenNGSPlatformServices.Initialize(new InitializationOptions()
+        {
+            AppId = "iboN4V3anKRnsKwgudonW0ESxGwJLNUz2rhN",
+            AppSecret = "YDwqSQ5Be0oGIpQJ6sPBlHLHveRTfC5p"
+        }, new SDKLogger());
+
+        AuthcationService.Instance.LoginCallback += (result) =>
+        {
+            SDKLog.Info($"[LoginCallback] ÓÃ»§[{result.Nickname}] µÇÂ¼³É¹¦ code:[{result}]");
+        };
+
+        AuthcationService.Instance.LoginResultCallback += (result) =>
+        {
+            SDKLog.Error($"[LoginCallback] µÇÂ¼´íÎó code:[{result}]");
+        };
+
+
+        AuthcationService.Instance.LoginOrRegisterByVerificationCode( 
+            OpenNGS.SDK.Auth.Models.VerificationType.Email,
+            EmailAddress,
+            VerifyCode);
+    }
+    [ContextMenu("TryLogout")]
+    private void TryLogout()
+    {
+        OpenNGSPlatformServices.Initialize(new InitializationOptions()
+        {
+            AppId = "iboN4V3anKRnsKwgudonW0ESxGwJLNUz2rhN",
+            AppSecret = "YDwqSQ5Be0oGIpQJ6sPBlHLHveRTfC5p"
+        }, new SDKLogger());
+
+        SDKLog.Info($"[Logout]");
+
+        AuthcationService.Instance.Logout();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        //TestLogin();
     }
 }
