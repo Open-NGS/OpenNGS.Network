153185aaa672094d2a12096153da8879b9425cb1
kanegu <kanegu@eegames.com>
2025-01-06 16:14:51 +0800

    dialogeffect结构添加

--- OpenNGS.Game.Systems/Generated/OpenNGS.Dialog.Data.cs
+++ OpenNGS.Game.Systems/Generated/OpenNGS.Dialog.Data.cs
@@ -105,6 +105,43 @@ namespace OpenNGS.Dialog.Data
         [global::ProtoBuf.ProtoMember(6, IsPacked = true)]
         public uint[] Effects { get; set; }
     }
+    //对话效果
+    [global::ProtoBuf.ProtoContract()]
+    public partial class DialogEffect : global::ProtoBuf.IExtensible
+{
+        private global::ProtoBuf.IExtension __pbn__extensionData;
+        global::ProtoBuf.IExtension global::ProtoBuf.IExtensible.GetExtensionObject(bool createIfMissing)
+        {
+        	return global::ProtoBuf.Extensible.GetExtensionObject(ref __pbn__extensionData, createIfMissing);
+        }
+        public DialogEffect()
+        {
+        	OnConstructor();
+        }
+        
+        partial void OnConstructor();
+
+        [global::ProtoBuf.ProtoMember(1, IsRequired = true)]
+        public uint EffectID { get; set; }
+        [global::ProtoBuf.ProtoMember(2)]
+        public uint Function { get; set; }
+        [global::ProtoBuf.ProtoMember(3)]
+        public NGSText DescInfo { get; set; }
+        [global::ProtoBuf.ProtoMember(4)]
+        public uint DescID { get; set; }
+        [global::ProtoBuf.ProtoMember(5)]
+        public uint Param1Type { get; set; }
+        [global::ProtoBuf.ProtoMember(6)]
+        public uint Param1 { get; set; }
+        [global::ProtoBuf.ProtoMember(7)]
+        public uint Param1b { get; set; }
+        [global::ProtoBuf.ProtoMember(8)]
+        public uint Param2Type { get; set; }
+        [global::ProtoBuf.ProtoMember(9)]
+        public uint Param2 { get; set; }
+        [global::ProtoBuf.ProtoMember(10)]
+        public uint Param2b { get; set; }
+    }
 
 }
 #pragma warning restore 0612, 0618, 1591, 3021
