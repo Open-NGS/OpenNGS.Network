98d85500af6b67a9765ffa3972224eeabf220907
孙楚晰 <arthursun@eegames.cn>
2025-02-18 18:00:31 +0800

    制作人界面更改

--- PLATFORM/Scripts/UICredits.cs
+++ PLATFORM/Scripts/UICredits.cs
@@ -1,4 +1,5 @@
 using Newtonsoft.Json;
+using OpenNGS.Credits;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
@@ -16,7 +17,7 @@ public class UICredits : MonoBehaviour
     public GameObject preb_ElementText;
     public string folderName;
     private GameObject m_curContent;
-
+    private List<string> m_urls = new List<string>();
     //测试使用本地文件
     public string jsonPath;
     public Sprite targetSprite;
@@ -41,11 +42,50 @@ public class UICredits : MonoBehaviour
             {
                 foreach (OpenNGS.Credits.Element element in root.Elements)
                 {
-                    UpdateUI(element);
+                    CheckImageType(element);
                 }
+                StartCoroutine(DownloadAllImages(root));
             }
         }
 
+    }
+    IEnumerator DownloadAllImages(OpenNGS.Credits.Root root)
+    {
+        foreach (string url in m_urls)
+        {
+            yield return StartCoroutine(DownloadImage(url));
+        }
+
+        // 所有图片下载完成后执行下一步操作
+        foreach (OpenNGS.Credits.Element element in root.Elements)
+        {
+            UpdateUI(element);
+        }
+    }
+    private void CheckImageType(OpenNGS.Credits.Element element) 
+    {
+        if (element.Type == "Image")
+        {
+            if (element.Title != null && element.Title != "")
+            {
+                string savePath = GetSavePath(element.Title);
+                if (!File.Exists(savePath))
+                {
+                    if (!m_urls.Contains(element.Title))
+                    {
+                        m_urls.Add(element.Title);
+                    }
+                }
+            }
+        }
+        if (element.Content != null)
+        {
+            foreach (OpenNGS.Credits.Element subElement in element.Content)
+            {
+                CheckImageType(subElement);
+            }
+        }
+        
     }
     private void UpdateUI(OpenNGS.Credits.Element element)
     {
@@ -82,7 +122,7 @@ public class UICredits : MonoBehaviour
                 {
                     if (folderName != null)
                     {
-                        StartCoroutine(DownloadAndShowImage(element.Title, image));
+                        LoadAndDisplayImage(GetSavePath(element.Title), image);
                     }
                 }
             }
@@ -120,18 +160,9 @@ public class UICredits : MonoBehaviour
         }
 
     }
-    IEnumerator DownloadAndShowImage(string url, Image image)
+    IEnumerator DownloadImage(string url)
     {
-        // 创建文件夹路径
-        string folderPath = Path.Combine(Application.persistentDataPath, folderName).Replace("\\", "/");
-        if (!Directory.Exists(folderPath))
-        {
-            Directory.CreateDirectory(folderPath); // 创建文件夹
-        }
-
-        // 图片保存路径
-        string fileName = Path.GetFileName(url);
-        string savePath = Path.Combine(folderPath, fileName).Replace("\\", "/");
+        string savePath = GetSavePath(url);
 
         // 下载图片
         using (UnityWebRequest request = UnityWebRequestTexture.GetTexture(url))
@@ -148,17 +179,33 @@ public class UICredits : MonoBehaviour
                 File.WriteAllBytes(savePath, imageBytes);
 
                 Debug.Log("图片已保存到: " + savePath);
-                Sprite sprite = Sprite.Create(
-                    texture,
-                    new Rect(0, 0, texture.width, texture.height),
-                    new Vector2(0.5f, 0.5f)); // 设置pivot为图片中心
-
-                image.sprite = sprite;
-                image.preserveAspect = true;
             }
         }
     }
+    private void LoadAndDisplayImage(string path,Image image)
+    {
+        // 读取本地图片
+        byte[] bytes = System.IO.File.ReadAllBytes(path);
+        Texture2D texture = new Texture2D(2, 2);
+        texture.LoadImage(bytes);
 
+        // 转换为Sprite并显示
+        Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
+        image.sprite = sprite;
+        image.preserveAspect = true;
+    }
+    private string GetSavePath(string url)
+    {
+        string folderPath = Path.Combine(Application.persistentDataPath, folderName).Replace("\\", "/");
+        if (!Directory.Exists(folderPath))
+        {
+            Directory.CreateDirectory(folderPath); // 创建文件夹
+        }
+        // 图片保存路径
+        string fileName = Path.GetFileName(url);
+        string savePath = Path.Combine(folderPath, fileName).Replace("\\", "/");
+        return savePath;
+    }
 }
 namespace OpenNGS.Credits
 {
