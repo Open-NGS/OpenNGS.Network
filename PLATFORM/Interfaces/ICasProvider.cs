09e31b708e79f405c0a8a98c1ceeaf88f658cf7c
kanegu <kanegu@eegames.com>
2025-06-10 11:38:01 +0800

    广告更改

--- PLATFORM/Interfaces/ICasProvider.cs
+++ PLATFORM/Interfaces/ICasProvider.cs
@@ -3,13 +3,12 @@ namespace OpenNGS.Platform
     public interface ICasProvider : IModuleProvider
     {
         void Initialize(string appKey, string strGameID, bool bTestMode = false);
-        //void Initialize(string strGameID, bool bTestMode = false);
         void SetMetaData(string strMetaCategory, string strMetaKey, string strMetaValue);
         void LoadBanner(string strAdUnitId, uint nBannerPosition);
         void ShowBannerAd(string strAdUnitId);
         void HideBannerAd(string strAdUnitId);
-        void LoadAd(string strAdUnitId);
-        void ShowAd(string strAdUnitId);
+        void LoadAd(string strAdUnitId, PlatformAdsType _typ);
+        void ShowAd(string strAdUnitId, PlatformAdsType _typ);
     }
 }
 
