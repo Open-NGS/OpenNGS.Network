e23b1487c226c26d9216d7905447847cf28a3d18
kanegu <kanegu@eegames.com>
2025-08-05 15:47:45 +0800

    对话静态数据变量类型更改

--- OpenNGS.Game.Systems/NgDialogSystem/DialogStaticData.cs
+++ OpenNGS.Game.Systems/NgDialogSystem/DialogStaticData.cs
@@ -1,14 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
-
 namespace OpenNGS.Systems
 {
     public static class DialogStaticData
     {
         public static Table<OpenNGS.Dialog.Data.DialogTalkConfig, uint, uint> DialogTalk = new Table<OpenNGS.Dialog.Data.DialogTalkConfig, uint,uint>((item) => { return item.DialogID; }, (item) => { return item.TalkID; }, false);
         public static Table<OpenNGS.Dialog.Data.DialogConfig, uint> Dialogue = new Table<OpenNGS.Dialog.Data.DialogConfig, uint>((item) => { return item.ID; }, false);
-        public static Table<OpenNGS.Dialog.Data.DialogChoiceConfig, uint> Choice = new Table<OpenNGS.Dialog.Data.DialogChoiceConfig, uint>((item) => { return item.ChoiceID; }, false);
-        public static Table<OpenNGS.Dialog.Data.ChoiceResultConfig, uint> ChoiceRes = new Table<OpenNGS.Dialog.Data.ChoiceResultConfig, uint>((item) => { return item.ResultID; }, false);
+        public static ListTableBase<OpenNGS.Dialog.Data.DialogChoiceConfig, uint> Choice = new ListTableBase<OpenNGS.Dialog.Data.DialogChoiceConfig, uint>((item) => { return item.ChoiceID; }, false);
+        public static ListTableBase<OpenNGS.Dialog.Data.ChoiceResultConfig, uint> ChoiceRes = new ListTableBase<OpenNGS.Dialog.Data.ChoiceResultConfig, uint>((item) => { return item.ResultID; }, false);
 
         public static void Init() { }
     }
